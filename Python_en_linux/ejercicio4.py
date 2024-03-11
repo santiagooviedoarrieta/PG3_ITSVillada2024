a385150e811f971febba47a11444ab271fe7051e
santiagooviedoarrieta <110994607+santiagooviedoarrieta@users.noreply.github.com>
2024-03-10 22:47:08 -0300

    Add files via upload

---
+++ Python_en_linux/ejercicio4.py
@@ -0,0 +1,6 @@
+list = [14, 32, 26, 64, 69]
+def lista_ascendente():
+    print("Lista desordenada ", list)
+    list.sort(reverse=True)
+    print("Lista ordenada de mayor a menor ", list)
+lista_ascendente()
