837379580a7faeefc758f26aea2ca1bd0e1800c0
santiagooviedoarrieta <110994607+santiagooviedoarrieta@users.noreply.github.com>
2024-03-15 11:31:08 -0300

    Delete Python_en_linux directory

--- Python_en_linux/ejercicio3.py
+++
@@ -1,9 +0,0 @@
-def rectangulo():
-    ancho = int(input("Anchura del rectángulo\n>>> "))
-    alto = int(input("Altura del rectángulo\n>>> "))
-    caracter = input("Ingrese el caracter a utilizar\n>>>")
-    for i in range(alto):
-        for j in range(ancho):
-            print(f' {caracter} ' , end ="" )
-        print()
-rectangulo()
--- Python_en_linux/ejercicio4.py
+++
@@ -1,6 +0,0 @@
-list = [14, 32, 26, 64, 69]
-def lista_ascendente():
-    print("Lista desordenada ", list)
-    list.sort(reverse=True)
-    print("Lista ordenada de mayor a menor ", list)
-lista_ascendente()
